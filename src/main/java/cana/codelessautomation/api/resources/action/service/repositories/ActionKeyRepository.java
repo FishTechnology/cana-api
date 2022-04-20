6c7a08084dd31f67ae61f9dd865afcfe44428b0f
sajanmje27 <sajanmje82@gmail.com>
2022-04-19 23:58:09 -0400

    added window

--- src/main/java/cana/codelessautomation/api/resources/action/service/repositories/ActionKeyRepository.java
+++ src/main/java/cana/codelessautomation/api/resources/action/service/repositories/ActionKeyRepository.java
@@ -3,5 +3,8 @@ package cana.codelessautomation.api.resources.action.service.repositories;
 import cana.codelessautomation.api.resources.action.service.repositories.daos.ActionKeyDao;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class ActionKeyRepository implements PanacheRepository<ActionKeyDao> {
 }
